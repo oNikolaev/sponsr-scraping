e05894aa0dfe35ae604daf6d6c55196762463f2e
o.nikolaev <o.nikolaev@rtinform.ru>
2022-09-16 12:16:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from urllib.error import HTTPError
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+def prepare_page():
+    html = urlopen("https://sponsr.ru/marahovsky/20985/Piramida_tehnichnyh_dikarei_trebuet_dat_ei_spasitelnyh_negrov/")
+    bsObj = BeautifulSoup(html, features="html.parser")
+    print(bsObj.h2.text)
+
+
+if __name__ == '__main__':
+    prepare_page()
